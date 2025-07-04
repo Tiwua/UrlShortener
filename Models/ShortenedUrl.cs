5c4ea6664e6b76d3acb162893f1eaeb1b0591300
Valentin Gaidarov <valentin.gaidarov@gmail.com>
2025-07-04 21:37:46 +0300

    db models added

---
+++ Models/ShortenedUrl.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Models
+{
+    public class ShortenedUrl
+    {
+        public ShortenedUrl()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreatedAt = DateTime.UtcNow;
+            this.AccessLogs = new List<UrlAccessLog>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(2048)]
+        public string OriginalUrl { get; set; } = null!;
+
+        [Required, MaxLength(10)]
+        public string ShortCode { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<UrlAccessLog> AccessLogs { get; set; } 
+    }
+}
---
+++ Models/UrlAccessLog.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Models
+{
+    public class UrlAccessLog
+    {
+        public UrlAccessLog()
+        {
+            this.Id = Guid.NewGuid();
+            this.AccessedAt = DateTime.UtcNow;
+        }
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string ShortenedUrlId { get; set; } = null!;
+        public ShortenedUrl ShortenedUrl { get; set; } = null!;
+
+        [Required]
+        public string IpAddress { get; set; } = null!;
+
+        [Required]
+        public DateTime AccessedAt { get; set; }
+
+    }
+}
